73f2248b2de29fc686bb86360863e7b45fd52ecf
Talha Ozleblebici <talha43gs@gmail.com>
2020-07-02 02:42:06 +0100

    Tasks Page test steps Updated

--- src/test/java/com/nextbasecrm/pages/tasksPage/TasksPage.java
+++ src/test/java/com/nextbasecrm/pages/tasksPage/TasksPage.java
@@ -1,7 +1,12 @@
 package com.nextbasecrm.pages.tasksPage;
 
 import com.nextbasecrm.pages.BasePage;
+import com.nextbasecrm.utilities.BrowserUtils;
+import com.nextbasecrm.utilities.Driver;
+import io.cucumber.java.en_old.Ac;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 
 public class TasksPage extends BasePage {
@@ -20,6 +25,36 @@ public class TasksPage extends BasePage {
 
     @FindBy(css="[data-bx-id='task-edit-submit'] span")
     public WebElement addButton;
+
+    @FindBy (xpath = "//span[@class='bx-calendar-button-text'][contains(text(),'Select')]")
+    public WebElement selectDateButton;
+
+    @FindBy (xpath = "//a[@class='bx-calendar-top-month']")
+    public WebElement monthSelect;
+
+    @FindBy (xpath = "//a[@class='bx-calendar-top-year']")
+    public WebElement yearSelect;
+
+
+
+
+
+    public void selectMonthInDropDown(String month){
+        monthSelect.click();
+        BrowserUtils.waitFor(1);
+        Driver.get().findElement(By.xpath("//div[@class='bx-calendar-month-content']//span[contains(text(),'"+month+"')]")).click();
+    }
+
+    public void selectYearInDropDown(String year){
+        yearSelect.click();
+        BrowserUtils.waitFor(1);
+        Driver.get().findElement(By.xpath("//div[@class='bx-calendar-year-content']//span[contains(text(),'"+year+"')]")).click();
+    }
+
+    public void selectDayOnTable (String day){
+        Driver.get().findElement(By.xpath("//div[@class='bx-calendar-range']//a[.='"+day+"']")).click();
+    }
+
 
 
 }
--- src/test/java/com/nextbasecrm/step_definitions/tasksPageStepDef/NewTasksStepDef.java
+++ src/test/java/com/nextbasecrm/step_definitions/tasksPageStepDef/NewTasksStepDef.java
@@ -5,7 +5,12 @@ import com.nextbasecrm.utilities.Driver;
 import com.nextbasecrm.utilities.Pages;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.cucumber.java.en_old.Ac;
+import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 import java.awt.dnd.DragGestureEvent;
 import java.util.List;
@@ -21,26 +26,47 @@ public class NewTasksStepDef {
         BrowserUtils.waitFor(3);
         Driver.get().switchTo().frame(Driver.get().findElement(By.cssSelector("[class='side-panel-iframe']")));
         pages.tasksPage().meetingNameInput.sendKeys(enterTaskData.get(0));
-        //Driver.get().switchTo().defaultContent();
-        //Driver.get().switchTo().frame(Driver.get().findElement(By.cssSelector("[class='bx-editor-iframe']")));
-       // pages.tasksPage().descriptionInput.click();
-        BrowserUtils.waitFor(2);
-        BrowserUtils.setAttribute(pages.tasksPage().descriptionInput,"value",enterTaskData.get(1));
-        BrowserUtils.waitFor(2);
-        ///pages.tasksPage().descriptionInput.sendKeys(enterTaskData.get(1));
-        //Driver.get().switchTo().defaultContent();
-       // Driver.get().switchTo().frame(Driver.get().findElement(By.cssSelector("[class='side-panel-iframe']")));
 
-       // pages.tasksPage().dateInput.click();
-       // pages.tasksPage().dateInput.sendKeys(enterTaskData.get(2));
-        BrowserUtils.setAttribute(pages.tasksPage().dateInput,"value",enterTaskData.get(2));
         BrowserUtils.waitFor(2);
+        BrowserUtils.setAttribute(pages.tasksPage().descriptionInput, "value", enterTaskData.get(1));
+        BrowserUtils.waitFor(2);
+
+        // Dikkat JS ile value attribute değer ata SELECT e tıklamadan kaydetmiyor!!
+
+//        JavascriptExecutor jse = (JavascriptExecutor)Driver.get();
+//        jse.executeScript("arguments[0].value='"+enterTaskData.get(2)+"';",pages.tasksPage().dateInput );
+        //BrowserUtils.setAttribute(pages.tasksPage().dateInput, "value", enterTaskData.get(2));
+        pages.tasksPage().dateInput.click();
+        BrowserUtils.waitFor(1);
+        pages.tasksPage().selectYearInDropDown(enterTaskData.get(4));
+        BrowserUtils.waitFor(2);
+        pages.tasksPage().selectMonthInDropDown(enterTaskData.get(3));
+        BrowserUtils.waitFor(2);
+        pages.tasksPage().selectDayOnTable(enterTaskData.get(2));
+        BrowserUtils.waitFor(1);
+        pages.tasksPage().selectDateButton.click();
+
         pages.tasksPage().addButton.click();
         Driver.get().switchTo().defaultContent();
 
     }
 
-    @Then("verify the task on the page with has following credentials: meeting name, description, dead line")
-    public void verifyTheTaskOnThePageWithHasFollowingCredentialsMeetingNameDescriptionDeadLine() {
+    @Then("verify the task on the page with has following credentials: {string} {string}")
+    public void verifyTheTaskOnThePageWithHasFollowingCredentials(String meetingName, String meetingDate) {
+
+        WebElement actualDateElement = Driver.get().findElement
+                (By.xpath("//a[contains(text(),'" + meetingName + "')]/../../following-sibling::td//span[contains(text(),'" + meetingDate + "')]"));
+        String actualDate = actualDateElement.getText();
+
+        meetingDate= meetingDate.replace(", ", ",");
+        Assert.assertEquals(meetingDate, actualDate);
+        // short way
+        Assert.assertTrue(actualDateElement.isDisplayed());
+
+
+
+
+
+
     }
 }
